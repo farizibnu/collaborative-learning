8fe232abda4cc3452c2470cd4e329d3d82ddd788
farizibnu <fariz2240@gmail.com>
2024-02-19 11:45:05 +0700

    dashboard update

---
+++ frontend/src/components/dashboard/DashboardDeadline.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const DashboardReport = () => {
+  return (
+  <>
+    <div className="card w-full mx-24">
+        <p className='text-3xl font-bold mb-10'>Upcoming Event</p>
+        <div className='flex justify-between'>
+            <div className='text-center rounded-lg bg-orange-100 p-3 w-56'>
+                <p>Komunitas yang diikuti</p>
+                <p className='text-2xl'>8</p>
+            </div>
+            <div className='text-center rounded-lg bg-blue-100 p-3 w-56'>
+                <p>Quiz Selesai</p>
+                <p className='text-2xl'>10</p>
+            </div>
+            <div className='text-center rounded-lg bg-green-100 p-3 w-56'>
+                <p>Komunitas yang diikuti</p>
+            </div>
+        </div>
+    </div>
+
+    </>
+  )
+}
+
+export default DashboardReport
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
+import DashboardReport from '../components/dashboard/DashboardReport'
+import DashboardDeadline from '../components/dashboard/DashboardDeadline' 
 
 const Home = () => {
   return (
-    <div className="mt-24">
-      <div className="flex flex-wrap lg:flex-nowrap justify-center">
-        Home
+    <div className="mt-4">
+      <div className="flex flex-wrap lg:flex-nowrap justify-center my-4">
+        <DashboardReport/>
         </div>
+        <div className="flex flex-wrap lg:flex-nowrap justify-center my-4">
+        <DashboardDeadline/>
+      </div>
     </div>
   )
 }
