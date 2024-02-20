d1175a3156bcc57e83c8f7c40c55286919524c36
farizibnu <fariz2240@gmail.com>
2024-02-20 22:48:59 +0700

    create profile page

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import ProfileHeader from '../components/profile/ProfileHeader';
+import ProfileInfo from '../components/profile/ProfileInfo';
+import ProfileDetail from '../components/profile/ProfileDetail';
 
 const Profile = () => {
   return (
-    <div>Profile</div>
+    <div>
+      <ProfileHeader/>
+      <div className='flex'>
+        <ProfileInfo/>
+        <ProfileDetail/>
+      </div>
+    </div>
   )
 }
 
