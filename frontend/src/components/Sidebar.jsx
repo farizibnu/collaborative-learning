5b512588b745be02032b08606d7faaefef6615ad
NItroF77 <142875762+RaihanFuadSyakir@users.noreply.github.com>
2024-02-29 09:38:32 +0700

    diff environtment

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import {Link, NavLink} from 'react-router-dom';
-import { AiOutlineSlackSquare, AiOutlineLeftSquare  } from "react-icons/ai";
-import {MdOutlineCancel} from 'react-icons/md';
-// import { SignInButton, SignedIn, SignedOut, UserButton, useUser } from "@clerk/clerk-react"
+import { Link, NavLink } from 'react-router-dom';
+import { AiOutlineSlackSquare, AiOutlineLeftSquare } from "react-icons/ai";
+import { MdOutlineCancel } from 'react-icons/md';
+import { SignInButton, SignedIn, SignedOut, UserButton, useUser } from "@clerk/clerk-react"
 
 import { useStateContext } from '../contexts/ContextProvider';
 
-import {links} from  '../data/dummy';
+import { links } from '../data/dummy';
 
 const Sidebar = () => {
   const activeMenu = true;
@@ -15,21 +15,21 @@ const Sidebar = () => {
 
   return (
     <div className='pl-3 h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto pb-10 bg-slate-950'>
-      {/* <div className='bg-white'>
-      <SignedOut>
+      <div className='bg-white'>
+        <SignedOut>
           <SignInButton />
-      </SignedOut>
-      <SignedIn>
+        </SignedOut>
+        <SignedIn>
           <UserButton afterSignOutUrl="/" />
-      </SignedIn>
-      </div> */}
+        </SignedIn>
+      </div>
       {activeMenu && (<>
         <div className='flex justify-between items-center pb-5'>
-          <Link to="/" onClick={() => {}} className='items-center ml-3 flex text-xl tracking-tight gap-3 mt-4 font-extrabold text-orange-400'>
-          <AiOutlineSlackSquare /><span className='text-white'>Cole</span>
+          <Link to="/" onClick={() => { }} className='items-center ml-3 flex text-xl tracking-tight gap-3 mt-4 font-extrabold text-orange-400'>
+            <AiOutlineSlackSquare /><span className='text-white'>Cole</span>
           </Link>
-          <button type='button' onClick={() => {}} className='text-2xl p-1 mx-2 hover:bg-light-gray mt-4 block md:hidden'>
-            <AiOutlineLeftSquare/>
+          <button type='button' onClick={() => { }} className='text-2xl p-1 mx-2 hover:bg-light-gray mt-4 block md:hidden'>
+            <AiOutlineLeftSquare />
           </button>
         </div>
         <div className=''>
@@ -42,7 +42,7 @@ const Sidebar = () => {
                 <NavLink
                   to={`/${link.url}`}
                   key={link.name}
-                  onClick={() => {}}
+                  onClick={() => { }}
                   className={({ isActive }) => isActive ? activeLink : normalLink}
                 >
                   {link.icon}
