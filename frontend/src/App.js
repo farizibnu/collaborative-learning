2b6a8332c16807414380cdb023ebdb24fd440962
farizibnu <fariz2240@gmail.com>
2024-03-04 09:26:16 +0700

    Add navigation links to dosen or mahasiswa

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react'
-import { BrowserRouter, Routes, Route } from  "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from  "react-router-dom";
 import { FiSettings }  from 'react-icons/fi';
 import {Sidebar, Button, Footer, Header, Navbar, Notification, UserProfile} from './components';
 import {Profile, ProfileEdit, Home, TanyaJawab, Achievement, CariTeman, Quiz, Login, Register, HomeDosen } from './pages/';
