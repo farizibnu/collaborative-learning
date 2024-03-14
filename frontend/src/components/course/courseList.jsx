cdc8a3d33b5f10622893e7a4fd60e4c5ca545a74
salmaep <salma.edyna.tif421@polban.ac.id>
2024-03-14 22:40:22 +0700

    add create course

--- frontend/src/components/course/courseList.jsx
+++ frontend/src/components/course/courseList.jsx
@@ -1,70 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
 import courses from "../../data/dataCourse.js";
 import { Link } from "react-router-dom";
 import { LuPencilLine } from "react-icons/lu";
 
 const CourseList = () => {
-  const role = "Student";
+  const role = "Teacher";
+
   return (
     <div className="grid grid-cols-3 gap-4">
       {courses.map((course) => (
-        <Link key={course.id} to={`/detail-course/${course.id}`}>
-          <div
-            key={course.id}
-            className="max-w-sm rounded overflow-hidden shadow-lg"
-          >
-            {role === "Teacher" ? (
-              <>
-                <div className="relative">
-                  <img
-                    className="w-full object-cover h-48"
-                    src={course.imageCourse}
-                    alt={course.title}
-                  />
-                  <a
-                    href="https://www.google.com/"
-                    class="absolute top-0 right-0 text-xl font-bold text-white p-2 rounded hover:bg-gray-800 m-2"
-                  >
-                    <LuPencilLine />
-                  </a>
-                </div>
-              </>
-            ) : (
-              <img
-                className="w-full object-cover h-48"
-                src={course.imageLecturer}
-                alt={course.title}
-              />
-            )}
-
-            <div className="px-6 py-4">
-              <div className="font-bold text-xl mb-2">{course.title}</div>
-              <hr className="border-1 border-black" />
-              <div className="flex items-center justify-between gap-2 cursor-pointer p-1 hover:bg-light-gray rounded-lg">
+        <div
+          key={course.id}
+          className="max-w-sm rounded overflow-hidden shadow-lg"
+        >
+          {role === "Teacher" ? (
+            <>
+              <div className="relative">
                 <img
-                  className="rounded-full w-8 h-8"
-                  src={course.imageLecturer}
-                  alt="user-profile"
+                  className="w-full object-cover h-48"
+                  src={course.imageCourse}
+                  alt={course.title}
                 />
-                <div className="w-full">
-                  <div className="flex w-full justify-between mt-1">
-                    <span className="ml-1 text-14">{course.instructor}</span>
-                    <p className="">
-                      <span className="inline-block truncate bg-blue-400 rounded-full px-3 py-1 text-xs text-white ml-2">
-                        {course.topic}
-                      </span>
-                    </p>
-                  </div>
-                  <div className="">
-                    <span className="ml-1 text-14">
-                      {course.positionLecturer}
+                <button className="absolute top-0 right-0 text-xl font-bold text-white p-2 rounded hover:bg-gray-800 m-2">
+                  <LuPencilLine />
+                </button>
+              </div>
+            </>
+          ) : (
+            <img
+              className="w-full object-cover h-48"
+              src={course.imageLecturer}
+              alt={course.title}
+            />
+          )}
+
+          <div className="px-6 py-4">
+            <Link key={course.id} to={`/detail-course/${course.id}`}>
+              <div className="font-bold text-xl mb-2">{course.title}</div>
+            </Link>
+            <hr className="border-1 border-black" />
+            <div className="flex items-center justify-between gap-2 cursor-pointer p-1 hover:bg-light-gray rounded-lg">
+              <img
+                className="rounded-full w-8 h-8"
+                src={course.imageLecturer}
+                alt="user-profile"
+              />
+              <div className="w-full">
+                <div className="flex w-full justify-between mt-1">
+                  <span className="ml-1 text-14">{course.instructor}</span>
+                  <p className="">
+                    <span className="inline-block truncate bg-blue-400 rounded-full px-3 py-1 text-xs text-white ml-2">
+                      {course.topic}
                     </span>
-                  </div>
+                  </p>
+                </div>
+                <div className="">
+                  <span className="ml-1 text-14">
+                    {course.positionLecturer}
+                  </span>
                 </div>
               </div>
             </div>
           </div>
-        </Link>
+        </div>
       ))}
     </div>
   );
--- frontend/src/pages/Course.jsx
+++ frontend/src/pages/Course.jsx
@@ -1,7 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
+import { Button, Modal } from "antd";
 import CourseList from "../components/course/courseList";
 
 const Course = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [courseData, setCourseData] = useState({
+    fullname: "",
+    shortname: "",
+    summary: "",
+  });
+
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleOk = async () => {
+    setIsModalOpen(false);
+    try {
+      // Tambah categoryid ke courseData
+      const dataToSend = { ...courseData, categoryid: 1 };
+      await createCourse(dataToSend);
+      // tambahan buat logika penanganan sukses disini
+    } catch (error) {
+      console.error("Error:", error);
+      // Handle error
+    }
+  };
+
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
+
+  const createCourse = async (courseData) => {
+    const response = await fetch("https://api.example.com/courses", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(courseData),
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    return data;
+  };
+
+  const handleChange = (e) => {
+    const { id, value } = e.target;
+    setCourseData((prevData) => ({
+      ...prevData,
+      [id]: value,
+    }));
+  };
+
   return (
     <div className="justify-items-start">
       <div className="bg-gradient-to-r from-indigo-300 to-purple-300 p-12 ">
@@ -12,14 +66,82 @@ const Course = () => {
       </div>
       <div className="flex justify-end mr-7 m-3">
         <button
-          data-modal-target="authentication-modal"
-          data-modal-toggle="authentication-modal"
+          onClick={showModal}
           className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
         >
-          Create Quiz
+          Create Course
         </button>
+        <Modal
+          title="Create Course"
+          open={isModalOpen}
+          onOk={handleOk}
+          onCancel={handleCancel}
+          footer={[
+            <Button key="back" onClick={handleCancel}>
+              Cancel
+            </Button>,
+            <Button
+              key="submit"
+              className="bg-blue-500 hover:bg-blue-700 text-white"
+              onClick={handleOk}
+            >
+              Submit
+            </Button>,
+          ]}
+        >
+          <form className="mt-4">
+            <div className="mb-4">
+              <label
+                className="block text-black text-base mb-2"
+                htmlFor="fullname"
+              >
+                Course Fullname
+              </label>
+              <input
+                className="appearance-none border rounded w-full py-3 px-3 leading-tight focus:shadow-outline focus:outline-gray-400"
+                id="fullname"
+                type="text"
+                placeholder="Course Fullname"
+                value={courseData.fullname}
+                onChange={handleChange}
+              />
+            </div>
+            <div className="mb-4">
+              <label
+                className="block text-black text-base mb-2"
+                htmlFor="shortname"
+              >
+                Course Shortname
+              </label>
+              <input
+                className="appearance-none border rounded w-full py-3 px-3 leading-tight focus:shadow-outline focus:outline-gray-400"
+                id="shortname"
+                type="text"
+                placeholder="Course Shortname"
+                value={courseData.shortname}
+                onChange={handleChange}
+              />
+            </div>
+            <div className="mb-4">
+              <label
+                className="block text-black text-base mb-2"
+                htmlFor="summary"
+              >
+                Summary Course
+              </label>
+              <textarea
+                className="appearance-none border rounded w-full py-3 px-3 leading-tight focus:shadow-outline focus:outline-gray-400"
+                id="summary"
+                type="text"
+                placeholder="Summary Course"
+                value={courseData.summary}
+                onChange={handleChange}
+              />
+            </div>
+          </form>
+        </Modal>
       </div>
-      <div class=" m-7 mt-5 grid grid-flow-col justify-stretch ...">
+      <div className="m-7 mt-5 grid grid-flow-col justify-stretch ...">
         <CourseList />
       </div>
     </div>
