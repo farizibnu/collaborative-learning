c567736e5b465dfd4ce206758bd2b53a312bb94a
salmaep <salma.edyna.tif421@polban.ac.id>
2024-03-14 23:04:32 +0700

    update pov student in course page

--- frontend/src/pages/Course.jsx
+++ frontend/src/pages/Course.jsx
@@ -3,6 +3,7 @@ import { Button, Modal } from "antd";
 import CourseList from "../components/course/courseList";
 
 const Course = () => {
+  const role = "Teacher";
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [courseData, setCourseData] = useState({
     fullname: "",
@@ -64,83 +65,91 @@ const Course = () => {
           Let’s join our best classes with our famous instructor
         </p>
       </div>
-      <div className="flex justify-end mr-7 m-3">
-        <button
-          onClick={showModal}
-          className="bg-orange-400 hover:bg-orange-600 text-white font-bold py-2 px-4 rounded"
-        >
-          Create Course
-        </button>
-        <Modal
-          title="Create Course"
-          open={isModalOpen}
-          onOk={handleOk}
-          onCancel={handleCancel}
-          footer={[
-            <Button key="back" onClick={handleCancel}>
-              Cancel
-            </Button>,
-            <Button
-              key="submit"
-              className="bg-blue-500 hover:bg-blue-700 text-white"
-              onClick={handleOk}
+      {role === "Teacher" ? (
+        <>
+          {" "}
+          <div className="flex justify-end mr-7 m-3">
+            <button
+              onClick={showModal}
+              className="bg-orange-400 hover:bg-orange-600 text-white font-bold py-2 px-4 rounded"
             >
-              Submit
-            </Button>,
-          ]}
-        >
-          <form className="mt-4">
-            <div className="mb-4">
-              <label
-                className="block text-black text-base mb-2"
-                htmlFor="fullname"
-              >
-                Course Fullname
-              </label>
-              <input
-                className="appearance-none border rounded w-full py-3 px-3 leading-tight focus:shadow-outline focus:outline-gray-400"
-                id="fullname"
-                type="text"
-                placeholder="Course Fullname"
-                value={courseData.fullname}
-                onChange={handleChange}
-              />
-            </div>
-            <div className="mb-4">
-              <label
-                className="block text-black text-base mb-2"
-                htmlFor="shortname"
-              >
-                Course Shortname
-              </label>
-              <input
-                className="appearance-none border rounded w-full py-3 px-3 leading-tight focus:shadow-outline focus:outline-gray-400"
-                id="shortname"
-                type="text"
-                placeholder="Course Shortname"
-                value={courseData.shortname}
-                onChange={handleChange}
-              />
-            </div>
-            <div className="mb-4">
-              <label
-                className="block text-black text-base mb-2"
-                htmlFor="summary"
-              >
-                Summary Course
-              </label>
-              <textarea
-                className="appearance-none border rounded w-full py-3 px-3 leading-tight focus:shadow-outline focus:outline-gray-400"
-                id="summary"
-                type="text"
-                placeholder="Summary Course"
-                value={courseData.summary}
-                onChange={handleChange}
-              />
-            </div>
-          </form>
-        </Modal>
-      </div>
+              Create Course
+            </button>
+            <Modal
+              title="Create Course"
+              open={isModalOpen}
+              onOk={handleOk}
+              onCancel={handleCancel}
+              footer={[
+                <Button key="back" onClick={handleCancel}>
+                  Cancel
+                </Button>,
+                <Button
+                  key="submit"
+                  className="bg-blue-500 hover:bg-blue-700 text-white"
+                  onClick={handleOk}
+                >
+                  Submit
+                </Button>,
+              ]}
+            >
+              <form className="mt-4">
+                <div className="mb-4">
+                  <label
+                    className="block text-black text-base mb-2"
+                    htmlFor="fullname"
+                  >
+                    Course Fullname
+                  </label>
+                  <input
+                    className="appearance-none border rounded w-full py-3 px-3 leading-tight focus:shadow-outline focus:outline-gray-400"
+                    id="fullname"
+                    type="text"
+                    placeholder="Course Fullname"
+                    value={courseData.fullname}
+                    onChange={handleChange}
+                  />
+                </div>
+                <div className="mb-4">
+                  <label
+                    className="block text-black text-base mb-2"
+                    htmlFor="shortname"
+                  >
+                    Course Shortname
+                  </label>
+                  <input
+                    className="appearance-none border rounded w-full py-3 px-3 leading-tight focus:shadow-outline focus:outline-gray-400"
+                    id="shortname"
+                    type="text"
+                    placeholder="Course Shortname"
+                    value={courseData.shortname}
+                    onChange={handleChange}
+                  />
+                </div>
+                <div className="mb-4">
+                  <label
+                    className="block text-black text-base mb-2"
+                    htmlFor="summary"
+                  >
+                    Summary Course
+                  </label>
+                  <textarea
+                    className="appearance-none border rounded w-full py-3 px-3 leading-tight focus:shadow-outline focus:outline-gray-400"
+                    id="summary"
+                    type="text"
+                    placeholder="Summary Course"
+                    value={courseData.summary}
+                    onChange={handleChange}
+                  />
+                </div>
+              </form>
+            </Modal>
+          </div>
+        </>
+      ) : (
+        <></>
+      )}
+
       <div className="m-7 mt-5 grid grid-flow-col justify-stretch ...">
         <CourseList />
       </div>
