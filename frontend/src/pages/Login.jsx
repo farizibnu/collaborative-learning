b7cd6ad191166ce07a5df0208b1ec2c2b8207dbb
FadhilRadja <Assydiq124@gmail.com>
2024-03-16 15:27:10 +0700

    Finishing Register and Login UI

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -5,6 +5,9 @@ import Cookies from 'universal-cookie';
 import axios from 'axios';
 import { loginMahasiswa, setTokenToOther } from '../lib/fetchData';
 import { VITE_BACKEND_CTB_URL } from '../lib/env';
+import { FaKey } from "react-icons/fa";
+import { AiOutlineSlackSquare, AiOutlineLeftSquare } from "react-icons/ai";
+
 const LoginPage = ({ onLogin }) => {
     const navigate = useNavigate();
     const [user, setUser] = useState(null);
@@ -63,51 +66,79 @@ const LoginPage = ({ onLogin }) => {
     }, [user])
 
     return (
-        <div className="flex justify-center items-center h-screen">
-            <form onSubmit={handleSubmit} className="w-full max-w-sm">
-                <p className='text-3xl mb-8 font-bold'>Login Page</p>
-                {error && <p className="text-red-500 mb-4">{error}</p>}
-                <div className="mb-4">
-                    <label htmlFor="email" className="block text-gray-700 text-sm font-bold mb-2">Email</label>
-                    <input
-                        type="email"
-                        id="email"
-                        name="email"
-                        value={formData.email}
-                        onChange={handleChange}
-                        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                        placeholder="Enter your email"
-                    />
-                </div>
-                <div className="mb-6">
-                    <label htmlFor="password" className="block text-gray-700 text-sm font-bold mb-2">Password</label>
-                    <input
-                        type="password"
-                        id="password"
-                        name="password"
-                        value={formData.password}
-                        onChange={handleChange}
-                        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                        placeholder="Enter your password"
-                    />
-                </div>
-                <div className="flex items-center justify-between">
-                    <button
-                        type="submit"
-                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                    >
-                        Log In
+        <>
+        <body class='bg-orange-100 min-h-screen'>
+            <div className='flex pt-5 px-10 pb-5'>
+                <AiOutlineSlackSquare style={{ fontSize: '40px' }} /><h1 className='font-extrabold text-4xl'>Colle</h1>
+                <Link to="/registerdashboard" className="ml-auto">
+                    <button className="bg-orange-400 text-white font-bold py-2 px-6 rounded-lg focus:outline-none focus:shadow-outline">
+                    Register
                     </button>
-                    <Link to={'/register'}>
-                        <p className='underline font-semibold text-sm'>Doesn't have an account? Register here</p>
-                    </Link>
-                </div>
-                <hr className='my-6 border-1 border-blue-300' />
-                <div className='border-2 border-blue-400 rounded-xl p-3 text-center font-bold hover:bg-blue-500 hover:text-white '>
-                    <button onClick={login}>Sign in with Google 🚀 </button> 
-                </div>
-            </form>
-        </div>
+                </Link>
+            </div>
+            
+            <p className='flex justify-center text-2xl font-bold my-10'>Login to Account</p>
+            <div className="flex justify-center">
+                <div className='bg-white border rounded-lg p-10'>
+                    <form onSubmit={handleSubmit} className="w-full max-w-sm">
+                        {error && <p className="text-red-500 mb-4">{error}</p>}
+                        <div className="mb-4">
+                            <input
+                                type="email"
+                                id="email"
+                                name="email"
+                                value={formData.email}
+                                onChange={handleChange}
+                                className="shadow appearance-none border rounded w-full py-3 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                placeholder="Email*"
+                            />
+                        </div>
+                        <div className="mb-6">
+                            <input
+                                type="password"
+                                id="password"
+                                name="password"
+                                value={formData.password}
+                                onChange={handleChange}
+                                className="shadow appearance-none border rounded w-full py-3 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                placeholder="Password*"
+                            />
+                        </div>
+                        <div className="grid grid-cols-2 gap-4 mb-6">
+                            <div className="flex items-center">
+                                <FaKey className="mr-2 text-yellow-500" />
+                                <p className='text-xs'>8 character minimum*</p>
+                            </div>
+                            <div className="flex items-center">
+                                <FaKey className="mr-2 text-yellow-500" />
+                                <p className='text-xs'>One lowercase character*</p>
+                            </div>
+                            <div className="flex items-center">
+                                <FaKey className="mr-2 text-yellow-500" />
+                                <p className='text-xs'>One uppercase character*</p>
+                            </div>
+                            <div className="flex items-center">
+                                <FaKey className="mr-2 text-yellow-500" />
+                                <p className='text-xs'>One number*</p>
+                            </div>
+                        </div>
+                        <div className="flex items-center justify-center">
+                            <button
+                                type="submit"
+                                className="outline bg-white hover:bg-orange-300 text-orange-300 hover:text-white font-bold py-2 px-6 rounded focus:outline-none focus:shadow-outline"
+                            >
+                                Log In
+                            </button>
+                        </div>
+                        <hr className='my-6 border-1 border-orange-300' />
+                        <div className='border-2 border-orange-300 rounded-xl p-3 text-center text-orange-300 font-bold hover:bg-orange-300 hover:text-white '>
+                            <button onClick={login}>Sign in with Google 🚀 </button> 
+                        </div>
+                    </form>
+                </div>  
+            </div>
+        </body>
+        </>
     );
 };
 
